c0f64095dde8bec557365108c376643d406b7c2f
baob <coder@onesandthrees.com>
2011-08-21 21:27:44 +0100

    all deletion and non-deletion cases covered

--- spec/lib/dedupedir_spec.rb
+++ spec/lib/dedupedir_spec.rb
@@ -5,7 +5,6 @@ describe "dedupedir.rb" do
 
   before(:all) do
     @test_dir = File.dirname(__FILE__) + '/../../tmp'
-    FileUtils.mkdir_p @test_dir
 
     def run_script
       Dedupedir.one_dir(@test_dir)
@@ -19,7 +18,11 @@ describe "dedupedir.rb" do
     end
   end
 
-  after(:all) do
+  before(:each) do
+    FileUtils.mkdir_p @test_dir
+  end
+
+  after(:each) do
     FileUtils.rm_rf @test_dir
   end
 
@@ -53,4 +56,54 @@ describe "dedupedir.rb" do
     File.should_not exist(f2)
     File.should_not exist(f3)
   end
+
+
+  it "does not delete when the files are in different directories" do
+    f1 = build_test_file('xab.mp3'  ,'test2') {|f| f.write('qqqqq') }
+    f2 = build_test_file('xab 2.mp3','test1') {|f| f.write('qqqqq') }
+    run_script
+    File.should exist(f1)
+    File.should exist(f2)
+  end
+
+  it "does not delete when the files have different content" do
+    f1 = build_test_file('xab.mp3'  ,'test1') {|f| f.write('qqqqq') }
+    f2 = build_test_file('xab 2.mp3','test1') {|f| f.write('11111') }
+    run_script
+    File.should exist(f1)
+    File.should exist(f2)
+  end
+
+  it "does not delete when the files have different length" do
+    f1 = build_test_file('xab.mp3'  ,'test1') {|f| f.write('qqqqq') }
+    f2 = build_test_file('xab 2.mp3','test1') {|f| f.write('qqqqqqqqqqq') }
+    run_script
+    File.should exist(f1)
+    File.should exist(f2)
+  end
+
+  it "does not delete when the file names differ by something other than a numeric suffix" do
+    f1 = build_test_file('xab.mp3'   ,'test1') {|f| f.write('qqqqq') }
+    f2 = build_test_file('xabkkk.mp3','test1') {|f| f.write('qqqqq') }
+    run_script
+    File.should exist(f1)
+    File.should exist(f2)
+  end
+
+  it "does not delete when the files do not share a prefix" do
+    f1 = build_test_file('xab.mp3'    ,'test1') {|f| f.write('qqqqq') }
+    f2 = build_test_file('hgghh 2.mp3','test1') {|f| f.write('qqqqq') }
+    run_script
+    File.should exist(f1)
+    File.should exist(f2)
+  end
+
+  it "does not delete when the files do not have identical file types" do
+    f1 = build_test_file('xab.mp3'  ,'test1') {|f| f.write('qqqqq') }
+    f2 = build_test_file('xab 2.txt','test1') {|f| f.write('qqqqq') }
+    run_script
+    File.should exist(f1)
+    File.should exist(f2)
+  end
+
 end
