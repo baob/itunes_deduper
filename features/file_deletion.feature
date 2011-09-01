c4724eea4738b087f16678bab5ddbbf28b408b75
baob <coder@onesandthrees.com>
2011-09-01 01:33:44 +0100

    file deletion feature re-done in end user language

--- features/file_deletion.feature
+++ features/file_deletion.feature
@@ -8,36 +8,27 @@ Feature: file deletion
     And I cd to "itunes_features"
 
   Scenario: deleting in root directory
-    Given a file named "track_1_by_x.mp3" with:
-       """
-       plinky-plonk
-       """
-    And a file named "track_1_by_x 2.mp3" which is a copy of "track_1_by_x.mp3"
+    Given a music file in the root directory
+    And a copy of that named with a numeric suffix
     When I run "dedupe"
     Then the stderr should be empty
-    And a file named "track_1_by_x.mp3" should exist
-    And a file named "track_1_by_x 2.mp3" should not exist
+    And the music file should exist
+    And the other file should not exist
 
   Scenario: deleting in sub directory
-    Given a file named "sub/track_1_by_x.mp3" with:
-       """
-       plinky-plonk
-       """
-    And a file named "sub/track_1_by_x 2.mp3" which is a copy of "sub/track_1_by_x.mp3"
+    Given a music file
+    And a copy of that named with a numeric suffix
     When I run "dedupe"
     Then the stderr should be empty
-    And a file named "sub/track_1_by_x.mp3" should exist
-    And a file named "sub/track_1_by_x 2.mp3" should not exist
+    And the music file should exist
+    And the other file should not exist
 
   Scenario: deleting in deep sub directory
-    Given a file named "sub1/sub2/sub3/sub4/track_1_by_x.mp3" with:
-       """
-       plinky-plonk
-       """
-    And a file named "sub1/sub2/sub3/sub4/track_1_by_x 2.mp3" which is a copy of "sub1/sub2/sub3/sub4/track_1_by_x.mp3"
+    Given a music file in a deep sub directory
+    And a copy of that named with a numeric suffix
     When I run "dedupe"
     Then the stderr should be empty
-    And a file named "sub1/sub2/sub3/sub4/track_1_by_x.mp3" should exist
-    And a file named "sub1/sub2/sub3/sub4/track_1_by_x 2.mp3" should not exist
+    And the music file should exist
+    And the other file should not exist
 
 
--- features/step_definitions/dedupe_steps.rb
+++ features/step_definitions/dedupe_steps.rb
@@ -1,33 +1,49 @@
 Given /^a music file$/ do
   @music_file = "itunes/track_1_by_x.mp3"
+  ensure_path_for @music_file
   Given "a file named \"#{@music_file}\" with:", "plinky-plonk"
 end   
 
+Given /^a music file in a deep sub directory$/ do
+  @music_file = "itunes/aaa/bbb/ccc/ddd/eee/fff/track_1_by_x.mp3"
+  ensure_path_for @music_file
+  Given "a file named \"#{@music_file}\" with:", "plinky-plonk"
+end
+
+Given /^a music file in the root directory$/ do
+  @music_file = "track_1_by_x.mp3"
+  Given "a file named \"#{@music_file}\" with:", "plinky-plonk"
+end
+
 Given /^a copy of that named with a numeric suffix$/ do
   @other_file = add_numeric_suffix(@music_file)
+  ensure_path_for @other_file
   Given "a file named \"#{@other_file}\" which is a copy of \"#{@music_file}\""
 end
 
 Given /^a copy of that named with a numeric suffix but with different content$/ do
   @other_file = add_numeric_suffix(@music_file)
+  ensure_path_for @other_file
   Given "a file named \"#{@other_file}\" with:", "trinky-tronk"
 end
 
 Given /^a copy of that named with a numeric suffix but with different size$/ do
   @other_file = add_numeric_suffix(@music_file)
+  ensure_path_for @other_file
   Given "a file named \"#{@other_file}\" with:", "trinky-tronk-boom"
 end
 
 Given /^a copy of that named with a non\-numeric suffix$/ do
   @other_file = add_suffix(@music_file,' X')
+  ensure_path_for @other_file
   Given "a file named \"#{@other_file}\" which is a copy of \"#{@music_file}\""
 end
 
 Given /^a copy of that named with a numeric suffix in a subdirectory$/ do
   path_parts = @music_file.split('/')
   path = File.join(*(path_parts[0..-2]+['sub1']))
-  in_current_dir { FileUtils.mkdir_p path }
   @other_file = File.join(path,add_numeric_suffix(path_parts.last))
+  in_current_dir { FileUtils.mkdir_p path }
   Given "a file named \"#{@other_file}\" which is a copy of \"#{@music_file}\""
 end
 
@@ -53,4 +69,9 @@ end
 
 def add_numeric_suffix(file_name)
   add_suffix(file_name,' 2')
+end
+
+def ensure_path_for(file_name)
+  path = file_name.split('/')[0..-2].join('/')
+  in_current_dir { FileUtils.mkdir_p path } if path && !path.size == 0
 end
